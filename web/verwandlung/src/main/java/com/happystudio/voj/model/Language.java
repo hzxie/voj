893c2971211b4dcfc7e8a5bc14782c46e508ca74
Haozhe Xie <root@haozhexie.com>
2014-10-17 10:45:08 +0800

    Complete UserDAO and complete unit testing of UserDAO.

---
+++ web/verwandlung/src/main/java/com/happystudio/voj/model/Language.java
@@ -0,0 +1,137 @@
+package com.happystudio.voj.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * 编程语言的Model.
+ * @author Xie Haozhe
+ */
+@Entity
+@Table(name = "voj_languages")
+public class Language {
+    /**
+     * 编程语言的默认构造函数.
+     */
+    public Language() { }
+    
+    /**
+     * 编程语言的构造函数.
+     * @param languageID - 编程语言的唯一标识符
+     * @param languageSlug - 编程语言的英文缩写
+     * @param languageName - 编程语言的名称
+     */
+    public Language(int languageID, String languageSlug, String languageName) {
+        this.languageID = languageID;
+        this.languageSlug = languageSlug;
+        this.languageName = languageName;
+    }
+    
+    /**
+     * 获取编程语言的唯一标识符.
+     * @return 编程语言的唯一标识符
+     */
+    public int getLanguageID() {
+        return languageID;
+    }
+    
+    /**
+     * 设置编程语言的唯一标识符.
+     * @param languageID - 编程语言的唯一标识符
+     */
+    public void setLanguageID(int languageID) {
+        this.languageID = languageID;
+    }
+    
+    /**
+     * 获取编程语言的英文缩写
+     * @return 编程语言的英文缩写
+     */
+    public String getLanguageSlug() {
+        return languageSlug;
+    }
+    
+    /**
+     * 设置编程语言的英文缩写.
+     * @param languageSlug - 编程语言的英文缩写
+     */
+    public void setLanguageSlug(String languageSlug) {
+        this.languageSlug = languageSlug;
+    }
+    
+    /**
+     * 获取编程语言的名称.
+     * @return 编程语言的名称
+     */
+    public String getLanguageName() {
+        return languageName;
+    }
+    
+    /**
+     * 设置编程语言的名称.
+     * @param languageName - 编程语言的名称
+     */
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+    
+    /**
+     * 获取用户列表(用于1-N关联).
+     * @return 用户列表
+     */
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * 设置用户列表.
+     * @param users - 用户列表
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return String.format("Language [ID=%d, Slug=%s, Name=%s]",
+                new Object[] { languageID, languageSlug, languageName });
+    }
+    
+    /**
+     * 编程语言的唯一标识符. 
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "language_id")
+    private int languageID;
+    
+    /**
+     * 编程语言的唯一英文简称.
+     */
+    @Column(name = "language_slug")
+    private String languageSlug;
+    
+    /**
+     * 编程语言的名称.
+     */
+    @Column(name = "language_name")
+    private String languageName;
+    
+    /**
+     * 用户列表(以便1-N关联).
+     */
+    @OneToMany(targetEntity = User.class, 
+                fetch = FetchType.LAZY, mappedBy = "preferLanguage")
+    private List<User> users = new ArrayList<User>();
+}
---
+++ web/verwandlung/src/main/java/com/happystudio/voj/model/User.java
@@ -0,0 +1,202 @@
+package com.happystudio.voj.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * 用户的Model.
+ * @author Xie Haozhe
+ */
+@Entity
+@Table(name = "voj_users")
+public class User {
+    /**
+     * User类的默认构造函数. 
+     */
+    public User() { }
+    
+    /**
+     * User类的构造函数.
+     * @param username - 用户名
+     * @param password - 密码
+     * @param email - 电子邮件地址
+     * @param userGroup - 用户组
+     * @param preferLanguage - 用户偏好语言
+     */
+    public User(String username, String password, String email, 
+                UserGroup userGroup, Language preferLanguage) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.userGroup = userGroup;
+        this.preferLanguage = preferLanguage;
+    }
+    
+    /**
+     * User类的构造函数.
+     * @param uid - 用户唯一标识符
+     * @param username - 用户名
+     * @param password - 密码
+     * @param email - 电子邮件地址
+     * @param userGroup - 用户组
+     * @param preferLanguage - 用户偏好语言
+     */
+    public User(int uid, String username, String password, String email, 
+                UserGroup userGroup, Language preferLanguage) {
+    	this.uid = uid;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.userGroup = userGroup;
+        this.preferLanguage = preferLanguage;
+    }
+    
+    /**
+     * 获取用户唯一标识符.
+     * @return 用户唯一标识符
+     */
+    public int getUid() {
+        return uid;
+    }
+    
+    /**
+     * 设置用户唯一标识符.
+     * @param uid - 用户唯一标识符
+     */
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    
+    /**
+     * 获取用户名.
+     * @return 用户名
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名.
+     * @param Username - 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    /**
+     * 获取密码(已采用MD5加密).
+     * @return 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码.
+     * @param password - 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取电子邮件地址.
+     * @return 电子邮件地址
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置电子邮件地址
+     * @param email - 电子邮件地址
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取用户组.
+     * @return 用户组对象
+     */
+    public UserGroup getUserGroup() {
+        return userGroup;
+    }
+
+    /**
+     * 设置用户组.
+     * @param userGroup - 用户组对象
+     */
+    public void setUserGroup(UserGroup userGroup) {
+        this.userGroup = userGroup;
+    }
+    
+    /**
+     * 获取用户偏好语言对象.
+     * @return 用户偏好语言对象
+     */
+    public Language getPreferLanguage() {
+        return preferLanguage;
+    }
+    
+    /**
+     * 设置用户偏好语言对象
+     * @param preferLanguage - 用户偏好语言对象
+     */
+    public void setPreferLanguage(Language preferLanguage) {
+        this.preferLanguage = preferLanguage;
+    }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return String.format("User: [Uid=%s, Username=%s, Password=%s, Email=%s, UserGroup={%s}, Language={%s}]", 
+                new Object[] { uid, username, password, email, userGroup, preferLanguage });
+    }
+
+    /**
+     * 用户的唯一标识符.
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "uid")
+    private int uid;
+    
+    /**
+     * 用户名.
+     */
+    @Column(name = "username")
+    private String username;
+
+    /**
+     * 密码(已采用MD5加密).
+     */
+    @Column(name = "password")
+    private String password;
+
+    /**
+     * 电子邮件地址.
+     */
+    @Column(name = "email")
+    private String email;
+    
+    /**
+     * 用户组对象.
+     */
+    @ManyToOne(targetEntity = UserGroup.class)
+    @JoinColumn(name = "user_group_id")
+    private UserGroup userGroup;
+    
+    /**
+     * 用户偏好语言对象.
+     */
+    @ManyToOne(targetEntity = Language.class)
+    @JoinColumn(name = "prefer_language_id", referencedColumnName="language_id")
+    private Language preferLanguage;
+}
