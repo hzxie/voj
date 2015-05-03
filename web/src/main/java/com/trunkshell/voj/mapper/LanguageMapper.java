5b578684b52fa2d9ae155add79786eaebfc450ff
Xie Haozhe <zjhzxhz@gmail.com>
2015-05-03 17:44:54 +0800

    Create CRUD method for LanguageMapper.
    Complete unit testing for LanguageMapper.
    
    Enable EhcacheCache for Mapper classes.

---
+++ web/src/main/java/com/trunkshell/voj/mapper/LanguageMapper.java
@@ -0,0 +1,71 @@
+package com.trunkshell.voj.mapper;
+
+import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.trunkshell.voj.model.Language;
+
+/**
+ * Language Data Access Object.
+ * @author Xie Haozhe
+ */
+@CacheNamespace(implementation = org.mybatis.caches.ehcache.EhcacheCache.class)
+public interface LanguageMapper {
+	/**
+	 * 通过编程语言的唯一标识符获取编程语言对象.
+	 * @param languageId - 编程语言的唯一标识符
+	 * @return 预期的编程语言对象或空引用
+	 */
+	@Select("SELECT * FROM voj_languages WHERE language_id = #{languageId}")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "languageId", column = "language_id"),
+		 @Result(property = "languageSlug", column = "language_slug"),
+		 @Result(property = "languageName", column = "language_name")
+	})
+	public Language getLanguageUsingId(int languageId);
+	
+	/**
+	 * 通过编程语言的唯一英文缩写获取编程语言对象.
+	 * @param languageSlug - 编程语言的唯一英文缩写
+	 * @return 预期的编程语言对象或空引用
+	 */
+	@Select("SELECT * FROM voj_languages WHERE language_slug = #{languageSlug}")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "languageId", column = "language_id"),
+		 @Result(property = "languageSlug", column = "language_slug"),
+		 @Result(property = "languageName", column = "language_name")
+	})
+	public Language getLanguageUsingSlug(String languageSlug);
+	
+	/**
+	 * 添加编程语言对象.
+	 * @param language - 待添加的编程语言对象 
+	 */
+	@Insert("INSERT INTO voj_languages (language_id, language_slug, language_name) VALUES (#{languageId}, #{languageSlug}, #{languageName})")
+	@Options(useGeneratedKeys = true, keyProperty = "language_id", flushCache = true)
+	public void createLanguage(Language language);
+	
+	/**
+	 * 更新编程语言对象.
+	 * @param language - 待更新的编程语言对象
+	 */
+	@Update("UPDATE voj_languages SET language_slug = #{languageSlug}, language_name = #{languageName} WHERE language_id = #{languageId}")
+	@Options(flushCache = true)
+	public void updateLanguage(Language language);
+	
+	/**
+	 * 删除编程语言对象.
+	 * @param languageId - 编程语言的唯一标识符
+	 */
+	@Delete("DELETE FROM voj_languages WHERE language_id = #{languageId}")
+	@Options(flushCache = true)
+	public void deleteLanguage(int languageId);
+}
---
+++ web/src/test/java/com/trunkshell/voj/mapper/LanguageMapperTest.java
@@ -0,0 +1,148 @@
+package com.trunkshell.voj.mapper;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trunkshell.voj.model.Language;
+
+/**
+ * LanguageMapper测试类.
+ * @author Xie Haozhe
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+@ContextConfiguration({"classpath:test-spring-context.xml"})
+public class LanguageMapperTest {
+	/**
+	 * 测试用例: 测试getLanguageUsingId(int)方法
+	 * 测试数据: C语言的编程语言唯一标识符
+	 * 预期结果: 返回C语言的编程语言对象
+	 */
+	@Test
+	public void testGetLanguageUsingIdExists() {
+		Language language = languageMapper.getLanguageUsingId(1);
+		Assert.assertNotNull(language);
+		
+		String languageName = language.getLanguageName();
+		Assert.assertEquals("C", languageName);
+	}
+	
+	/**
+	 * 测试用例: 测试getLanguageUsingId(int)方法
+	 * 测试数据: 不存在的编程语言唯一标识符
+	 * 预期结果: 返回空引用
+	 */
+	@Test
+	public void testGetLanguageUsingIdNotExists() {
+		Language language = languageMapper.getLanguageUsingId(0);
+		Assert.assertNull(language);
+	}
+	
+	/**
+	 * 测试用例: 测试getLanguageUsingSlug(String)方法
+	 * 测试数据: C语言的编程语言唯一英文缩写
+	 * 预期结果: 返回C语言的编程语言对象
+	 */
+	@Test
+	public void testGetLanguageUsingSlugExists() {
+		Language language = languageMapper.getLanguageUsingSlug("text/x-c");
+		Assert.assertNotNull(language);
+		
+		String languageName = language.getLanguageName();
+		Assert.assertEquals("C", languageName);
+	}
+	
+	/**
+	 * 测试用例: 测试getLanguageUsingSlug(String)方法
+	 * 测试数据: 不存在的编程语言唯一英文缩写
+	 * 预期结果: 返回空引用
+	 */
+	@Test
+	public void testGetLanguageUsingSlugNotExists() {
+		Language language = languageMapper.getLanguageUsingSlug("Not-Exists");
+		Assert.assertNull(language);
+	}
+	
+	/**
+	 * 测试用例: 测试createLanguage(Language)方法
+	 * 测试数据: 合法的数据集
+	 * 预期结果: 数据插入操作成功完成
+	 */
+	@Test
+	public void testCreateLanguageNormal() {
+		Language language = new Language("text/x-php", "PHP");
+		languageMapper.createLanguage(language);
+	}
+	
+	/**
+	 * 测试用例: 测试createLanguage(Language)方法
+	 * 测试数据: 不合法的数据集(过长的编程语言英文缩写)
+	 * 预期结果: 抛出DataIntegrityViolationException异常
+	 */
+	@Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+	public void testCreateLanguageUsingTooLongSlug() {
+		Language language = new Language("TooLongLanguageSlug", "Invalid Langauge");
+		languageMapper.createLanguage(language);
+	}
+	
+	/**
+	 * 测试用例: 测试updateLanguage(Language)方法
+	 * 测试数据: 合法的数据集, 且数据表中存在对应ID的记录
+	 * 预期结果: 数据更新操作成功完成
+	 */
+	@Test
+	public void testUpdateLanguageNormal() {
+		Language language = languageMapper.getLanguageUsingId(1);
+		Assert.assertNotNull(language);
+		
+		language.setLanguageName("C Language");
+		languageMapper.updateLanguage(language);
+		
+		language = languageMapper.getLanguageUsingId(1);
+		Assert.assertEquals("C Language", language.getLanguageName());
+	}
+	
+	/**
+	 * 测试用例: 测试updateLanguage(Language)方法
+	 * 测试数据: 合法的数据集, 但数据表中不存在该编程语言
+	 * 预期结果: 方法正常执行, 未影响数据表中的数据
+	 */
+	@Test
+	public void testUpdateLanguageNotExists() {
+		Language language = new Language(0, "not-exist", "Not Exist");
+		languageMapper.updateLanguage(language);
+	}
+	
+	/**
+	 * 测试用例: 测试deleteLanguage(int)方法
+	 * 测试数据: Ruby语言的编程语言唯一标识符
+	 * 预期结果: 数据删除操作成功完成
+	 */
+	@Test
+	public void testDeleteLanguageExists() {
+		languageMapper.deleteLanguage(6);
+	}
+	
+	/**
+	 * 测试用例: 测试deleteLanguage(int)方法
+	 * 测试数据: 不存在的编程语言唯一标识符
+	 * 预期结果: 方法正常执行, 未影响数据表中的数据
+	 */
+	@Test
+	public void testDeleteLanguageNotExists() {
+		languageMapper.deleteLanguage(0);
+	}
+	
+	/**
+	 * 待测试的LanguageMapper对象.
+	 */
+	@Autowired
+	private LanguageMapper languageMapper;
+}
