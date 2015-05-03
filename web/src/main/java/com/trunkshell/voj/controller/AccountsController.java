9dc511f499d3e4d8146fe9d5ab2f91f9da590e66
Haozhe Xie <root@haozhexie.com>
2015-05-03 16:04:57 +0800

    Initially complete login function.
    Create UserGroupMapper and unit testing cases.

--- web/src/main/java/com/trunkshell/voj/controller/AccountsController.java
+++ web/src/main/java/com/trunkshell/voj/controller/AccountsController.java
@@ -1,16 +1,22 @@
 package com.trunkshell.voj.controller;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.trunkshell.voj.model.User;
+import com.trunkshell.voj.service.UserService;
 import com.trunkshell.voj.util.HttpRequestParser;
 import com.trunkshell.voj.util.HttpSessionParser;
 
@@ -73,6 +79,52 @@ public class AccountsController {
         return true;
     }
     
+    /**
+	 * 处理用户的异步登录请求.
+	 * @param username - 用户名
+	 * @param password - 密码(已使用MD5加密)
+	 * @param request - Http Servlet Request对象
+	 * @return 一个包含若干标志位的JSON对象
+	 */
+	@RequestMapping(value = "/login.action", method = RequestMethod.POST)
+	public @ResponseBody HashMap<String, Boolean> loginAction(
+			@RequestParam(value="username", required=true) String username,
+			@RequestParam(value="password", required=true) String password,
+			@RequestParam(value="rememberMe", required=true) boolean isAutoLoginAllowed,
+			HttpServletRequest request) {
+		String ipAddress = HttpRequestParser.getRemoteAddr(request);
+		HashMap<String, Boolean> result = userService.isAccountValid(username, password);
+		logger.info(String.format("User: [Username=%s] tried to log in at %s", new Object[] {username, ipAddress}));
+		if ( result.get("isSuccessful") ) {
+			User user = userService.getUserUsingUsernameOrEmail(username);
+			System.out.println(user);
+			getSession(request, user, isAutoLoginAllowed);
+		}
+		return result;
+	}
+
+	/**
+	 * 为登录的用户创建Session.
+	 * @param request - HttpServletRequest对象
+	 * @param user - 一个User对象, 包含用户的基本信息
+	 * @param isAutoLoginAllowed - 是否保存登录状态
+	 */
+	private void getSession(HttpServletRequest request, User user, boolean isAutoLoginAllowed) {
+		HttpSession session = request.getSession();
+		session.setAttribute("isLoggedIn", true);
+		session.setAttribute("isAutoLoginAllowed", isAutoLoginAllowed);
+		session.setAttribute("uid", user.getUid());
+		
+		String ipAddress = HttpRequestParser.getRemoteAddr(request);
+		logger.info(String.format("%s logged in at %s", new Object[] {user, ipAddress}));
+	}
+    
+    /**
+     * 自动注入的UserService对象.
+     */
+    @Autowired
+    private UserService userService;
+	
     /**
      * 日志记录器.
      */
--- web/src/main/java/com/trunkshell/voj/mapper/UserMapper.java
+++ web/src/main/java/com/trunkshell/voj/mapper/UserMapper.java
@@ -1,10 +1,19 @@
 package com.trunkshell.voj.mapper;
 
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.trunkshell.voj.model.User;
+import com.trunkshell.voj.model.UserGroup;
 
+/**
+ * User Data Access Object.
+ * @author Xie Haozhe
+ */
 public interface UserMapper {
 	/**
 	 * 通过用户唯一标识符获取用户对象.
@@ -12,6 +21,9 @@ public interface UserMapper {
 	 * @return 预期的用户对象或空引用
 	 */
 	@Select("SELECT * FROM voj_users WHERE uid = #{uid}")
+	@Results(value = {
+		@Result(property = "userGroup", column = "user_group_id", javaType=UserGroup.class, one = @One(select="com.trunkshell.voj.mapper.UserGroupMapper.getUserGroupUsingId"))
+	})
 	public User getUserUsingUid(@Param("uid") long uid);
 	
 	/**
@@ -21,4 +33,15 @@ public interface UserMapper {
 	 */
 	@Select("SELECT * FROM voj_users WHERE username = #{username}")
 	public User getUserUsingUsername(@Param("username") String username);
+	
+	/**
+	 * 通过电子邮件地址获取用户对象.
+	 * @param username - 用户名
+	 * @return 预期的用户对象或空引用
+	 */
+	@Select("SELECT * FROM voj_users WHERE email = #{email}")
+	public User getUserUsingEmail(@Param("email") String email);
+	
+	@Options(useGeneratedKeys = true, keyProperty = "uid")
+	public void createUser(User user);
 }
