09b886028ca28a64fa3041df83b29a8b45c1e05b
Xie Haozhe <zjhzxhz@gmail.com>
2015-05-05 12:37:58 +0800

    Complete Retrieve methods for SubmissionMapper.
    Complete unit testing for Retrieve methods in SubmissionMapper.
    
    Update the data in the Database in order to test the Mapper classes better.

--- web/src/main/java/com/trunkshell/voj/mapper/SubmissionMapper.java
+++ web/src/main/java/com/trunkshell/voj/mapper/SubmissionMapper.java
@@ -1,5 +1,7 @@
 package com.trunkshell.voj.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Options;
@@ -42,4 +44,95 @@ public interface SubmissionMapper {
 		 @Result(property = "code", column = "submission_code"),
 	})
 	public Submission getSubmission(@Param("submissionId") long submissionId);
+	
+	/**
+     * 通过试题唯一标识符获取某个范围内的所有试题.
+     * @param offset - 试题唯一标识符的起始编号
+     * @param limit - 每次加载评测记录的数量
+     * @return 某个范围内的所有试题
+     */
+	@Select("SELECT * FROM voj_submissions WHERE submission_id <= #{submissionId} ORDER BY submission_id DESC LIMIT #{limit}")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "submissionId", column = "submission_id"),
+		 @Result(property = "problem", column = "problem_id", javaType=Problem.class, one = @One(select="com.trunkshell.voj.mapper.ProblemMapper.getProblem")),
+		 @Result(property = "user", column = "uid", javaType=User.class, one = @One(select="com.trunkshell.voj.mapper.UserMapper.getUserUsingUid")),
+		 @Result(property = "language", column = "language_id", javaType=Language.class, one = @One(select="com.trunkshell.voj.mapper.LanguageMapper.getLanguageUsingId")),
+		 @Result(property = "submitTime", column = "submission_submit_time"),
+		 @Result(property = "usedTime", column = "submission_used_time"),
+		 @Result(property = "usedMemory", column = "submission_used_memory"),
+		 @Result(property = "judgeResult", column = "submission_judge_result", javaType=JudgeResult.class, one = @One(select="com.trunkshell.voj.mapper.JudgeResultMapper.getJudgeResultUsingSlug")),
+		 @Result(property = "judgeScore", column = "submission_judge_score")
+	})
+	public List<Submission> getSubmissions(@Param("submissionId") long offset, @Param("limit") int limit);
+	
+	/**
+     * 获取某个用户对某个试题的提交记录.
+     * @param problemId - 试题的唯一标识符
+     * @param uid - 用户的唯一标识符
+     * @param limit - 每次加载评测记录的数量
+     * @return 某个用户对某个试题的提交记录
+     */
+	@Select("SELECT * FROM voj_submissions WHERE problem_id = #{problemId} AND uid = #{uid} ORDER BY submission_id DESC LIMIT #{limit}")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "submissionId", column = "submission_id"),
+		 @Result(property = "submitTime", column = "submission_submit_time"),
+		 @Result(property = "judgeResult", column = "submission_judge_result", javaType=JudgeResult.class, one = @One(select="com.trunkshell.voj.mapper.JudgeResultMapper.getJudgeResultUsingSlug")),
+	})
+	public List<Submission> getSubmissionUsingProblemIdAndUserId(@Param("problemId") long problemId, @Param("uid") long uid, @Param("limit") int limit);
+	
+	/**
+	 * 获取某个用户在某个试题ID区间段内的最新的评测结果.
+	 * @param uid - 用户的唯一标识符
+	 * @param problemIdLowerBound - 试题ID区间的下界
+	 * @param problemIdUpperBound - 试题ID区间的上界
+	 * @return 某个试题ID区间段内的最新的评测结果
+	 */
+	@Select("SELECT problem_id, submission_judge_result FROM ( SELECT * FROM voj_submissions ORDER BY submission_id DESC ) s WHERE uid = #{uid} AND problem_id >= #{problemIdLowerBound} AND problem_id <= #{problemIdUpperBound} GROUP BY problem_id")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "problem", column = "problem_id", javaType=Problem.class, one = @One(select="com.trunkshell.voj.mapper.ProblemMapper.getProblem")),
+		 @Result(property = "judgeResult", column = "submission_judge_result", javaType=JudgeResult.class, one = @One(select="com.trunkshell.voj.mapper.JudgeResultMapper.getJudgeResultUsingSlug"))
+	})
+	public List<Submission> getLatestSubmissionOfProblems(@Param("uid") long uid, @Param("problemIdLowerBound") long problemIdLowerBound, @Param("problemIdUpperBound") long problemIdUpperBound);
+	
+	/**
+	 * 获取某个用户在某个试题ID区间段内的通过的评测结果.
+	 * @param uid - 用户的唯一标识符
+	 * @param problemIdLowerBound - 试题ID区间的下界
+	 * @param problemIdUpperBound - 试题ID区间的上界
+	 * @return 某个试题ID区间段内的通过的评测结果
+	 */
+	@Select("SELECT problem_id, submission_judge_result FROM voj_submissions WHERE uid = #{uid} AND problem_id >= #{problemIdLowerBound} AND problem_id <= #{problemIdUpperBound} AND submission_judge_result = 'AC' GROUP BY problem_id")
+	@Options(useCache = true)
+	@Results({
+		 @Result(property = "problem", column = "problem_id", javaType=Problem.class, one = @One(select="com.trunkshell.voj.mapper.ProblemMapper.getProblem")),
+		 @Result(property = "judgeResult", column = "submission_judge_result", javaType=JudgeResult.class, one = @One(select="com.trunkshell.voj.mapper.JudgeResultMapper.getJudgeResultUsingSlug"))
+	})
+	public List<Submission> getAcceptedSubmissionOfProblems(@Param("uid") long uid, @Param("problemIdLowerBound") long problemIdLowerBound, @Param("problemIdUpperBound") long problemIdUpperBound);
+	
+	/**
+     * 获取某个用户通过(Accpeted)提交记录的数量.
+     * @param uid - 用户的唯一标识符
+     * @return 某个用户通过(Accpeted)提交记录的数量
+     */
+	@Select("SELECT COUNT(DISTINCT problem_id) FROM voj_submissions WHERE uid = #{uid} AND submission_judge_result = 'AC'")
+	@Options(useCache = true)
+	public long getAcceptedSubmissionUsingUserId(@Param("uid") long uid);
+	
+	/**
+     * 获取某个用户全部提交记录的数量.
+     * @param uid - 用户的唯一标识符
+     * @return 某个用户全部提交记录的数量
+     */
+	@Select("SELECT COUNT(*) FROM voj_submissions WHERE uid = #{uid}")
+	@Options(useCache = true)
+	public long getTotalSubmissionUsingUserId(@Param("uid") long uid);
+	
+	public void createSubmission(Submission submission);
+	
+	public void updateSubmission(Submission submission);
+	
+	public void deleteSubmission(long submissionId);
 }
--- web/src/test/java/com/trunkshell/voj/mapper/SubmissionMapperTest.java
+++ web/src/test/java/com/trunkshell/voj/mapper/SubmissionMapperTest.java
@@ -1,5 +1,8 @@
 package com.trunkshell.voj.mapper;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.trunkshell.voj.model.Problem;
 import com.trunkshell.voj.model.Submission;
 
 /**
@@ -19,10 +23,197 @@ import com.trunkshell.voj.model.Submission;
 @TransactionConfiguration(defaultRollback = true)
 @ContextConfiguration({"classpath:test-spring-context.xml"})
 public class SubmissionMapperTest {
+	/**
+	 * 测试用例: 测试getSubmission(long)方法
+	 * 测试数据: Problem#1000的提交记录的唯一标识符
+	 * 预期结果: 返回预期的Submission对象
+	 */
 	@Test
 	public void testGetSubmissionExists() {
-		Submission submission = submissionMapper.getSubmission(1);
-		System.out.println(submission);
+		Submission submission = submissionMapper.getSubmission(1000);
+		Assert.assertNotNull(submission);
+		
+		Problem problem = submission.getProblem();
+		long problemId = problem.getProblemId();
+		Assert.assertEquals(1000, problemId);
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmission(long)方法
+	 * 测试数据: 不存在的的提交记录唯一标识符
+	 * 预期结果: 返回空引用
+	 */
+	@Test
+	public void testGetSubmissionNotExists() {
+		Submission submission = submissionMapper.getSubmission(0);
+		Assert.assertNull(submission);
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissions(long, int)方法
+	 * 测试数据: 获取ID从1010起始的10次提交(ID From 1010 to 1000)
+	 * 预期结果: 返回提交列表(共4次提交)
+	 */
+	@Test
+	public void testGetSubmissionsFrom1010WithLimit10() {
+		List<Submission> submissions = submissionMapper.getSubmissions(1010, 10);
+		Assert.assertEquals(4, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		long submissionId = firstSubmission.getSubmissionId();
+		Assert.assertEquals(1003, submissionId);
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissions(long, int)方法
+	 * 测试数据: 获取ID从1003起始的2次提交
+	 * 预期结果: 返回提交列表(共2次提交)
+	 */
+	@Test
+	public void testGetSubmissionsFrom1003WithLimit2() {
+		List<Submission> submissions = submissionMapper.getSubmissions(1003, 2);
+		Assert.assertEquals(2, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		long submissionId = firstSubmission.getSubmissionId();
+		Assert.assertEquals(1003, submissionId);
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissions(long, int)方法
+	 * 测试数据: 获取ID从100起始的10次提交(ID From 100 to 90)
+	 * 预期结果: 返回空提交列表
+	 */
+	@Test
+	public void testGetSubmissionsFrom100WithLimit10() {
+		List<Submission> submissions = submissionMapper.getSubmissions(100, 10);
+		Assert.assertEquals(0, submissions.size());
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissionUsingProblemIdAndUserId(long, long, int)方法
+	 * 测试数据: 获取试题ID#1000和用户ID#1000的最新1次提交
+	 * 预期结果: 返回预期的提交列表(共1次提交)
+	 */
+	@Test
+	public void testGetSubmissionUsingExistingProblemIdAndExistingUserId() {
+		List<Submission> submissions = submissionMapper.getSubmissionUsingProblemIdAndUserId(1000, 1000, 1);
+		Assert.assertEquals(1, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		long submissionId = firstSubmission.getSubmissionId();
+		Assert.assertEquals(1001, submissionId);
+		
+		String judgeResultSlug = firstSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("WA", judgeResultSlug);
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissionUsingProblemIdAndUserId(long, long, int)方法
+	 * 测试数据: 使用不存在的试题唯一标识符
+	 * 预期结果: 返回空提交列表
+	 */
+	@Test
+	public void testGetSubmissionUsingNotExistingProblemIdAndExistingUserId() {
+		List<Submission> submissions = submissionMapper.getSubmissionUsingProblemIdAndUserId(0, 1000, 10);
+		Assert.assertEquals(0, submissions.size());
+	}
+	
+	/**
+	 * 测试用例: 测试getSubmissionUsingProblemIdAndUserId(long, long, int)方法
+	 * 测试数据: 使用不存在的用户唯一标识符
+	 * 预期结果: 返回空提交列表
+	 */
+	@Test
+	public void testGetSubmissionUsingExistingProblemIdAndNotExistingUserId() {
+		List<Submission> submissions = submissionMapper.getSubmissionUsingProblemIdAndUserId(1000, 0, 10);
+		Assert.assertEquals(0, submissions.size());
+	}
+	
+	/**
+	 * 测试用例: 测试getLatestSubmissionOfProblems(long, long, long)方法
+	 * 测试数据: 获取用户ID#1000在试题ID区间[1000, 1010)的最新评测结果
+	 * 预期结果: 返回预期的提交列表(试题ID#1000 - WA, 试题ID#1001 - AC)
+	 */
+	@Test
+	public void testGetLatestSubmissionOfProblemsWithUser1000() {
+		List<Submission> submissions = submissionMapper.getLatestSubmissionOfProblems(1000, 1000, 1010);
+		Assert.assertEquals(2, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		String firstJudgeResultSlug = firstSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("WA", firstJudgeResultSlug);
+		
+		Submission secondSubmission = submissions.get(1);
+		String secondJudgeResultSlug = secondSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("AC", secondJudgeResultSlug);
+	}
+	
+	/**
+	 * 测试用例: 测试getLatestSubmissionOfProblems(long, long, long)方法
+	 * 测试数据: 获取用户ID#1001在试题ID区间[1000, 1010)的最新评测结果
+	 * 预期结果: 返回预期的提交列表(试题ID#1000 - CE)
+	 */
+	@Test
+	public void testGetLatestSubmissionOfProblemsWithUser1001() {
+		List<Submission> submissions = submissionMapper.getLatestSubmissionOfProblems(1001, 1000, 1010);
+		Assert.assertEquals(1, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		String firstJudgeResultSlug = firstSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("CE", firstJudgeResultSlug);
+	}
+	
+	/**
+	 * 测试用例: 测试getAcceptedSubmissionOfProblems(long, long, long)方法
+	 * 测试数据: 获取用户ID#1000在试题ID区间[1000, 1010)的通过评测结果
+	 * 预期结果: 返回预期的提交列表(试题ID#1000 - AC, 试题ID#1001 - AC)
+	 */
+	@Test
+	public void testGetAcceptedSubmissionOfProblemsWithUser1000() {
+		List<Submission> submissions = submissionMapper.getAcceptedSubmissionOfProblems(1000, 1000, 1010);
+		Assert.assertEquals(2, submissions.size());
+		
+		Submission firstSubmission = submissions.get(0);
+		String firstJudgeResultSlug = firstSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("AC", firstJudgeResultSlug);
+		
+		Submission secondSubmission = submissions.get(1);
+		String secondJudgeResultSlug = secondSubmission.getJudgeResult().getJudgeResultSlug();
+		Assert.assertEquals("AC", secondJudgeResultSlug);
+	}
+	
+	/**
+	 * 测试用例: 测试getAcceptedSubmissionOfProblems(long, long, long)方法
+	 * 测试数据: 获取用户ID#1001在试题ID区间[1000, 1010)的通过评测结果
+	 * 预期结果: 返回空提交列表
+	 */
+	@Test
+	public void testGetAcceptedSubmissionOfProblemsWithUser1001() {
+		List<Submission> submissions = submissionMapper.getAcceptedSubmissionOfProblems(1001, 1000, 1010);
+		Assert.assertEquals(0, submissions.size());
+	}
+	
+	/**
+	 * 测试用例: 测试getAcceptedSubmissionUsingUserId(long)方法
+	 * 测试数据: 获取用户ID#1001通过的提交次数
+	 * 预期结果: 返回用户ID#1001通过的提交次数(共2次)
+	 */
+	@Test
+	public void testGetAcceptedSubmissionUsingUserIdWithUser1000() {
+		long acceptedSubmission = submissionMapper.getAcceptedSubmissionUsingUserId(1000);
+		Assert.assertEquals(2, acceptedSubmission);
+	}
+	
+	/**
+	 * 测试用例: 测试getTotalSubmissionUsingUserId(long)方法
+	 * 测试数据: 获取用户ID#1001总提交次数
+	 * 预期结果: 返回用户ID#1001总提交次数(共4次)
+	 */
+	@Test
+	public void testGetTotalSubmissionUsingUserIdWithUser1000() {
+		long acceptedSubmission = submissionMapper.getTotalSubmissionUsingUserId(1000);
+		Assert.assertEquals(3, acceptedSubmission);
 	}
 	
 	/**
