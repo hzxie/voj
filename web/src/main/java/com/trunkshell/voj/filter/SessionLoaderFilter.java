fe408fc7f31790f75e2edc50675610285d5b482c
gdsglgf <wzguofulgz@126.com>
2015-05-20 13:28:38 +0800

    refactoring session loader filter

--- web/src/main/java/com/trunkshell/voj/filter/SessionLoaderFilter.java
+++ web/src/main/java/com/trunkshell/voj/filter/SessionLoaderFilter.java
@@ -40,8 +40,7 @@ public class SessionLoaderFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 	           FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		session = req.getSession();
+		session = ((HttpServletRequest) request).getSession();
 		chain.doFilter(request, response);
 	}
 }
