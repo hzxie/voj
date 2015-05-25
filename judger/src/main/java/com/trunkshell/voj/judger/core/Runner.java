7def6865a52ae84b9eca899e378e71959d4bfc49
Xie Haozhe <zjhzxhz@gmail.com>
2015-05-25 10:40:48 +0800

    Use JNI in Judger.Core.Runner.
    Tested in Linux, futher developing in Windows is needed.

--- judger/src/main/java/com/trunkshell/voj/judger/core/Runner.java
+++ judger/src/main/java/com/trunkshell/voj/judger/core/Runner.java
@@ -1,11 +1,47 @@
 package com.trunkshell.voj.judger.core;
 
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.trunkshell.voj.judger.util.NativeLibraryLoader;
+
 /**
  * 本地程序执行器, 用于执行本地应用程序.
  * 包括编译器(gcc)以及用户提交的代码所编译出的程序.
  * 
  * @author Xie Haozhe
  */
+@Component
 public class Runner {
+	/**
+	 * 获取程序运行结果.
+	 * @param commandLine - 待执行程序的命令行
+	 * @param inputFilePath - 输入文件路径(可为NULL)
+	 * @param outputFilePath - 输出文件路径(可为NULL)
+	 * @param timeLimit - 时间限制(单位ms, 0表示不限制)
+	 * @param memoryLimit - 内存限制(单位KB, 0表示不限制)
+	 * @return 一个包含程序运行结果的Map<String, Object>对象
+	 */
+	public native Map<String, Object> getRuntimeResult(String commandLine, 
+			String inputFilePath, String outputFilePath, int timeLimit, int memoryLimit);
 	
+	/**
+	 * 日志记录器.
+	 */
+	private static final Logger logger = LogManager.getLogger(Runner.class);
+	
+	/**
+	 * Load Native Library.
+	 */
+	static {
+		try {
+			NativeLibraryLoader.loadLibrary("JudgerCore");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.catching(ex);
+		}
+	}
 }
---
+++ judger/src/test/java/com/trunkshell/voj/judger/core/RunnerTest.java
@@ -0,0 +1,45 @@
+package com.trunkshell.voj.judger.core;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 评测机调度器的测试类.
+ * 
+ * @author Xie Haozhe
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+@ContextConfiguration({"classpath:test-spring-context.xml"})
+public class RunnerTest {
+	/**
+	 * 测试用例: 
+	 * 测试数据: 
+	 * 预期结果: 
+	 */
+	@Test
+	public void testGetRuntimeResult() {
+		String commandLine = "";
+		String inputFilePath = null;
+		String outputFilePath = null;
+		int timeLimit = 1000;
+		int memoryLimit = 32768;
+		
+		Map<String, Object> runtimeResult = runner.getRuntimeResult(commandLine, 
+				inputFilePath, outputFilePath, timeLimit, memoryLimit);
+	}
+	
+	/**
+	 * 待测试的Runner对象.
+	 */
+	@Autowired
+	private Runner runner;
+}
