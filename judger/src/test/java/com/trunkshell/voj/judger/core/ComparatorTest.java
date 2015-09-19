07a09ad0e6b1846d631f6df0a9e8ec0f21a02471
Haozhe Xie <root@haozhexie.com>
2015-09-19 16:15:56 +0800

    Remove @TransactionManager annotation since it's deprecated in Spring 4.2.

--- judger/src/test/java/com/trunkshell/voj/judger/core/ComparatorTest.java
+++ judger/src/test/java/com/trunkshell/voj/judger/core/ComparatorTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
@@ -24,7 +23,6 @@ import java.io.IOException;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-@TransactionConfiguration(defaultRollback = true)
 @ContextConfiguration({ "classpath:test-spring-context.xml" })
 public class ComparatorTest {
     /**
--- web/src/test/java/com/trunkshell/voj/web/mapper/EmailValidationMapperTest.java
+++ web/src/test/java/com/trunkshell/voj/web/mapper/EmailValidationMapperTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.trunkshell.voj.web.model.EmailValidation;
@@ -20,7 +19,6 @@ import java.util.Date;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-@TransactionConfiguration(defaultRollback = true)
 @ContextConfiguration({"classpath:test-spring-context.xml"})
 public class EmailValidationMapperTest {
     /**
