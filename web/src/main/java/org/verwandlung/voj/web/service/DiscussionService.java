751e356a2624406d9699adc5a1d5f50e52f1e80e
Haozhe Xie <cshzxie@gmail.com>
2017-01-14 23:44:13 +0800

    Remove unused packages in DiscussionService.

--- web/src/main/java/org/verwandlung/voj/web/service/DiscussionService.java
+++ web/src/main/java/org/verwandlung/voj/web/service/DiscussionService.java
@@ -3,7 +3,6 @@ package org.verwandlung.voj.web.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import jdk.nashorn.api.scripting.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
