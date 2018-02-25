90f43668bb9da312af3827f62597e48599f1589f
Haozhe Xie <root@haozhexie.com>
2018-02-25 20:07:07 +0800

    Complete leaderboard for ACM.

--- web/src/main/java/org/verwandlung/voj/web/mapper/ContestSubmissionMapper.java
+++ web/src/main/java/org/verwandlung/voj/web/mapper/ContestSubmissionMapper.java
@@ -19,6 +19,13 @@ public interface ContestSubmissionMapper {
 	List<ContestSubmission> getSubmissionsOfContest(@Param("contestId") long contestId);
 
 	/**
+	 * 获取某场竞赛AC的提交记录.
+	 * @param contestId - 竞赛的唯一标识符
+	 * @return 包含AC竞赛提交记录(ContestSubmission)的List对象
+	 */
+	List<ContestSubmission> getAcceptedSubmissionsOfContest(@Param("contestId") long contestId);
+
+	/**
 	 * 创建提交记录.
 	 * @param contestSubmission - 待创建的提交记录
 	 */
